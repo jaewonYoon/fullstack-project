ce83df5b85b094a7f35ec21193eee04be410b50a
jaewonYoon <21300492@handong.edu>
2019-05-10 01:07:29 +0900

    add Router, axios

---
+++ reactPractice/poketimes/src/components/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import axios from "axios";
+class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    //async function
+    //.then methods fired when previous promises completed.
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      //if we don't have any posts
+      <div className="center">No posts yet</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
+
+export default Home;
